213e661b3190a0b10fce807b1d1128fae917665c
wuyang-pc <wuyangv@gmail.com>
2018-10-17 11:46:44 +0800

    wuyang-pc 1017-1146

--- src/com/java/instance/chapter8/demo2/PrivateOverride.java
+++ src/com/java/instance/chapter8/demo2/PrivateOverride.java
@@ -11,8 +11,8 @@ public class PrivateOverride {
     public static void main(String[] args){
         PrivateOverride privateOverride = new Derived();
         privateOverride.f();
-        Derived derived = new Anny();
-        derived.f();
+        //Derived derived = new Anny();
+        //derived.f();
     }
 }
 
